57e07a41dd6b4957a3d6fc2eff107ce7e66df283
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-09-29 00:36:05 +0600

    Module 3.5

---
+++ Practice Day/module 3.5/Y_Range_sum_query.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+s = [0] * (n + 1)
+for i in range(1, n + 1):
+    s[i] = s[i - 1] + arr[i - 1]
+for _ in range(m):
+    x, y = map(int, input().split())
+    q = s[y] - s[x - 1]
+    print(q)
